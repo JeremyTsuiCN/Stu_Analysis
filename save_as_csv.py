f026de1d290d7e306a1618f134b23c9be6ad8397
jeremy <564174495@qq.com>
2020-01-16 15:36:10 +0800

    new function: generate a miss students list

--- save_as_csv.py
+++ save_as_csv.py
@@ -5,10 +5,9 @@ import requests,json,csv,gevent,re
 
 work = Queue()
 
-#get this request url
-# first_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/chats/panels/10/modules?current_path=%2Fpanel%2Fconductor'
+#rail way workspace
 first_url2 = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/chats/panels/10/modules?current_path=%2F%252Fpanel%252Fconductor'
-#need a proxy agent
+
 proxy = {
 'http': '117.94.182.64'
 }
@@ -20,16 +19,15 @@ User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:72.0) Gecko/2010010
 Accept: application/json, text/plain, */*
 Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
 Accept-Encoding: gzip, deflate, br
-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU3OTA4MDY0MiwiaWF0IjoxNTc5MDczNDQyLCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.qojbSUKNEzgC5m5jHMVHequVPLMVSjaHoBmBBSb-hGs
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU3OTE2MzkxOSwiaWF0IjoxNTc5MTU2NzE5LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.sylpJsjqCeZPzqd8FbLU6EEOr2Dgnx09viFZO1Roiac
 Connection: keep-alive
 Referer: https://prod.pandateacher.com/ninth-studio-future-railway/frontend/
-Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; SERVERID=d1972a243b4e02b6a433b3790390cf1e|1579073898|1579055059; locale=zh-CN; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU3OTA4MDY0MiwiaWF0IjoxNTc5MDczNDQyLCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.qojbSUKNEzgC5m5jHMVHequVPLMVSjaHoBmBBSb-hGs
+Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; SERVERID=3847ab526202584eee1fed82128f885c|1579157019|1579156718; locale=zh-CN; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU3OTE2MzkxOSwiaWF0IjoxNTc5MTU2NzE5LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.sylpJsjqCeZPzqd8FbLU6EEOr2Dgnx09viFZO1Roiac
 TE: Trailers'''
 
-#generate a headers
+
 headers = dict([line.split(": ", 1) for line in todayheaders.split("\n")])
 
-#send requests then get reponse
 res = requests.get(first_url2, headers = headers, proxies = proxy)
 
 
@@ -127,5 +125,29 @@ else:
     print('empty special student list')
 
 
+#check miss students
+try:
+    with open(abs_path + 'all_names.csv') as allwx_file:
+        reader = csv.reader(allwx_file)
+        all_names_dict = dict((line[0],line[1]) for line in reader)
+
+    with open(abs_path + 'complete_data.csv') as actwx_file:
+        reader = csv.reader(actwx_file)
+        act_wxnames = [line[1] for line in reader]
+    with open(abs_path + 'stu_not_in_list/miss_stu.csv', 'w') as miss_file:
+        writer = csv.writer(miss_file)
+        for wxname,nickname in all_names_dict.items():
+            if wxname not in act_wxnames:
+                writer.writerow([wxname,nickname])
+    
+except FileNotFoundError:
+    print('You miss some documents......please check\n\n')
+
+except UnicodeDecodeError:
+    print('CSV document can\'t not be encoded by <Unicode> , \n need to save as other encode format')
+    
+
+
 
 print('Separate students at different stages')
+
