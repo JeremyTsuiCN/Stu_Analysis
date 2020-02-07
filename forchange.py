da3874378468e139a08a54d970f39f8a6d7d9756
jeremy <564174495@qq.com>
2020-02-07 19:35:35 +0800

    add readme.xmind
    fix bug  now_class need to be an int, be using output csv file name
    
    fix bugs about miss students, indent

--- forchange.py
+++ forchange.py
@@ -5,10 +5,26 @@
 
 __author__ = 'Jeremy Tsui'
 
+
+import os
+
+
+def createpath(newpath):
+    try:
+        os.makedirs(newpath)
+    except FileExistsError:
+        print(newpath+' exists \n')
+    finally:
+        return newpath
+
+abs_path = createpath('/Users/apple/Desktop/op_savecsv/')
+sep_f_path = createpath(abs_path+'separate_csvfile/')
+
+
 from gevent import monkey
 monkey.patch_all()
 from queue import Queue
-import requests, json, csv, gevent, re, math, os
+import requests, json, csv, gevent, re, math
 
 #get ready for the first requests
 first_url2 = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/chats/panels/10/modules?current_path=%2F%252Fpanel%252Fconductor'
@@ -20,33 +36,21 @@ User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:72.0) Gecko/2010010
 Accept: application/json, text/plain, */*
 Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
 Accept-Encoding: gzip, deflate, br
-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4MDk3MzE5MywiaWF0IjoxNTgwOTY1OTkzLCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.ZeAGKjLHyvO7tIX3SotYxn9r_F6xf4QlBN5_Av5OW5c
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4MTczODk2NSwiaWF0IjoxNTgxNzMxNzY1LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.skrmsx0ByD4f2g20s5XNfbTh-sVo4Pf5625RrWEvqqE
 Connection: keep-alive
 Referer: https://prod.pandateacher.com/ninth-studio-future-railway/frontend/
-Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; SERVERID=3847ab526202584eee1fed82128f885c|1580968576|1580958788; locale=zh-CN; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4MDk3MzE5MywiaWF0IjoxNTgwOTY1OTkzLCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.ZeAGKjLHyvO7tIX3SotYxn9r_F6xf4QlBN5_Av5OW5c
+Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; SERVERID=d1972a243b4e02b6a433b3790390cf1e|1581731775|1581731764; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4MTczODk2NSwiaWF0IjoxNTgxNzMxNzY1LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.skrmsx0ByD4f2g20s5XNfbTh-sVo4Pf5625RrWEvqqE; locale=zh-CN
 TE: Trailers'''
 
 headers = dict([line.split(": ", 1) for line in todayheaders.split("\n")])
 
 #special students' keyword in nicknames 
-keyword = ['忙忙', '考试', '妈妈', '放弃', '特殊','请假', '周一', '周二', '周三', '周四', '周五', '周六', '周日', '周末','不回复', '自节奏'] 
+keyword = ['忙忙', '考试', '放弃','请假', '周一', '周二', '周三', '周四', '周五', '周六', '周日', '周末','不回复', '自节奏'] 
 
 
-def createpath(newpath):
-    try:
-        os.makedirs(newpath)
-    except FileExistsError:
-        print(newpath+' exists \n')
-    finally:
-        return newpath
-
-
-
-abs_path = createpath('/Users/apple/Desktop/op_savecsv/')
 work = Queue()
 
 #some students need personal conversation 
-
 private_stu = []
 normal_stu = []
 
@@ -58,8 +62,6 @@ end_num = 0
 now_class = 0
 # '山脚'level needs to learn 6 tests, others needs 5
 miss_num = 5 
-
-
 
 
 #determine class level, return the answer to standard_target
@@ -87,7 +89,8 @@ def get_wx():
     #should update this url periodically to avoid potential errors.
     for studata in get_allstu_group:
         wxid = studata['wxid']
-        status_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/buddy/wxid_sqin1b8axidb22/{}?current_path=%2Fpanel%2Fconductor&is_group=false'.format(wxid)
+        # status_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/buddy/wxid_sqin1b8axidb22/{}?current_path=%2Fpanel%2Fconductor&is_group=false'.format(wxid)
+        status_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/buddy/wxid_ii0h8a2zxqud22/{}?current_path=%2Fpanel%2Fconductor&is_group=false'.format(wxid)
         work.put_nowait(status_url)
 
     #wanna print schedule, so I need this num to check presentage
@@ -97,10 +100,15 @@ def get_wx():
 #before we catch info, we need to prevent railway system missing data, so we 
 #supplement complete table of data from 'all_names.csv'
 #get complete standard data( without details and notedname)
-with open(abs_path + 'all_names.csv') as allwx_file:
-    reader = csv.reader(allwx_file)
-    all_names_dict = dict((line[0],line[8]) for line in reader)
-
+def get_sys_data():
+    try:
+        with open(abs_path + 'all_names.csv') as allwx_file:
+            reader = csv.reader(allwx_file)
+            return dict((line[0],line[8]) for line in reader)
+    except FileNotFoundError:
+        print('You miss some documents......please check\n\n')
+    except UnicodeDecodeError:
+        print('CSV document can\'t not be encoded by <Unicode> , \n need to save as other encode format')
 
 
 #each task , increases COUNT_INDEX to print percentage of getting data's schedule
@@ -124,9 +132,16 @@ def crawler():
             try:
                 level =  miss_num - int(all_names_dict[weixin]) + ((now_class - 1) * 5)
                 progress = ''
-                distance = '2'
+                nickname += ' 无数据'
             except KeyError:
                 print(weixin+'NOT FOUND')
+                return None
+            finally:
+                if int(all_names_dict[weixin]) == 0:
+                    distance = 300
+                else:
+                    progress = ''
+                    distance = 0
 
         except UnboundLocalError:
             return None  
@@ -150,15 +165,16 @@ def start_task():
         task = gevent.spawn(crawler)
         task_list.append(task)
     gevent.joinall(task_list)
+    
 
-def write_in():
+def output_csv():
     with open(abs_path + 'students_in_rail.csv', 'w') as crawlfile:
         writer = csv.writer(crawlfile)
         writer.writerow(['nickname', 'wxid', 'details', 'level', 'progress', 'learning status'])
         writer.writerows(normal_stu)
     print('\nWrite in all students in this train successful!!')
+    act_wxnames = dict( (line[1],line[0]) for line in normal_stu )
 
-def output_csv():
     if private_stu:
         with open(abs_path + 'per_con.csv', 'w', newline='') as perfile:
             writer = csv.writer(perfile)
@@ -166,42 +182,23 @@ def output_csv():
             writer.writerows(private_stu)
         print('Write in special students successful!!')
         pri_wxid = [line[1] for line in private_stu]
-
     else:
-        print('None special student catched!!!  Maybe you should update 【start_url】')
+        print('None special student catched!!! If keyword only 特殊, should updata url')
 
-    try:
-        with open(abs_path + 'all_names.csv') as allwx_file:
-            reader = csv.reader(allwx_file)
-            #all_names.csv 的顺序是 wxid nickname 
-            all_names_dict = dict( (line[0],line[1]) for line in reader ) 
-
-        with open(abs_path + 'students_in_rail.csv') as actwx_file:
-            reader = csv.reader(actwx_file)
-            #列车的顺序是 nickname wxid
-            act_wxnames = dict( (line[1],line[0]) for line in reader ) 
             
-        #Gather all students you can contact
-        effective_stu = set(pri_wxid + [names for names in act_wxnames])
+    #Gather all students you can contact
+    effective_stu = set(pri_wxid + [wx for wx in act_wxnames])
+    missfile_path = createpath(abs_path + 'miss/')
+    #student can't be contacted, write in [miss_stu.csv] 
+    #all_names.csv 的顺序是 0wxid 1nickname 
+    with open(missfile_path+'miss_stu.csv', 'w') as miss_file:
+        writer = csv.writer(miss_file)
+        for wxid in all_names_dict:
+            if wxid not in effective_stu:
+                writer.writerow([wxid,all_names_dict[wxid]])
 
-        missfile_path = createpath(abs_path + 'miss/')
+    print('All csvs had generated successfully~~~')
 
-        #student can't be contacted, write in [miss_stu.csv] 
-        with open(missfile_path+'miss_stu.csv', 'w') as miss_file:
-            writer = csv.writer(miss_file)
-            for wxname in all_names_dict:
-                if wxname not in effective_stu:
-                    writer.writerow([wxname,all_names_dict[wxname]])
-
-        print('All csvs had generated successfully~~~')
-        
-    except FileNotFoundError:
-        print('You miss some documents......please check\n\n')
-
-    except UnicodeDecodeError:
-        print('CSV document can\'t not be encoded by <Unicode> , \n need to save as other encode format')
-
-    print('Separate 3 status students in csv~\n')
 
 # One class has to set 12 groups students
 level_bottom = []
@@ -216,8 +213,12 @@ level_forth_high = []
 level_fifth_low = []
 level_fifth_high = []
 level_over = []
+
+# add a error limit, avoid system info error, need to check by myself
+suspected = []
+
 #create a main list to save all groups, name a list, and it will get a 'n' to mark it's index.
-all_list = [level_bottom,level_first_low ,level_first_high,level_second_low,level_second_high,level_third_low,level_third_high,level_forth_low,level_forth_high,level_fifth_low,level_fifth_high,level_over]
+all_list = [level_bottom, level_first_low, level_first_high, level_second_low, level_second_high, level_third_low, level_third_high, level_forth_low, level_forth_high, level_fifth_low, level_fifth_high, level_over, suspected]
 
 work2 = Queue()
 #put these groups into queue
@@ -226,66 +227,63 @@ for all in all_list:
 
 #read csv's data by line, distinguish, then add to target list
 def add2list(end_num_list):
-    with open(abs_path+'students_in_rail.csv') as raildata:
-        data = raildata.readlines()
     level_num = (int(now_class)-1)*5
 
+    for line in normal_stu:
+        if int(line[3]) == level_num:
+            level_bottom.append(line)
 
-    for eachline in data:
-        line = eachline.split(',')
-        try:
-            #bottom level 
-            if int(line[3]) == level_num:
-                level_bottom.append(line)
+        elif int(line[3]) == level_num+1:
+            if int(line[4]) < end_num_list[1]:
+                level_first_low.append(line)
+            else:
+                level_first_high.append(line)
 
-            elif int(line[3]) == level_num+1:
-                if int(line[4]) < end_num_list[1]:
-                    level_first_low.append(line)
-                else:
-                    level_first_high.append(line)
+        elif int(line[3]) == level_num+2:
+            if int(line[4]) < end_num_list[2]:
+                level_second_low.append(line)
+            else:
+                level_second_high.append(line)
 
-            elif int(line[3]) == level_num+2:
-                if int(line[4]) < end_num_list[2]:
-                    level_second_low.append(line)
-                else:
-                    level_second_high.append(line)
+        elif int(line[3]) == level_num+3:
+            if int(line[4])<end_num_list[3]:
+                level_third_low.append(line)
+            else:
+                level_third_high.append(line)
 
-            elif int(line[3]) == level_num+3:
-                if int(line[4])<end_num_list[3]:
-                    level_third_low.append(line)
-                else:
-                    level_third_high.append(line)
-
-            elif int(line[3]) == level_num+4:
-                if int(line[4]) < end_num_list[4]:
-                    level_forth_low.append(line)
-                else:
-                    level_forth_high.append(line)
-
-            elif int(line[3]) == level_num+5:
-                if int(line[4]) < end_num_list[5]:
-                    level_fifth_low.append(line)
-                else:
+        elif int(line[3]) == level_num+4:
+            if int(line[4]) < end_num_list[4]:
+                level_forth_low.append(line)
+            else:
+                level_forth_high.append(line)
+        #in the last level, to reduce system error rate, need to check in 'all_names_dict' again
+        elif int(line[3]) == level_num+5:
+            if int(line[4]) < end_num_list[5]-10:
+                level_fifth_low.append(line)
+            elif end_num_list[5]-10 < int(line[4]) < end_num_list[5]:
+                if int(all_names_dict[line[1]]) == 0:
                     level_fifth_high.append(line)
+                else:
+                    suspected.append(line)
+            else:
+                level_fifth_high.append(line)
 
-            elif int(line[3]) > level_num+5:
-                level_over.append(line)
-        except:
-            continue
+        elif int(line[3]) > level_num+5:
+            level_over.append(line)
 
 
-sep_f_path = createpath(abs_path+'separate_csvfile/')
+
 def write_each(filename, index):
-
     #to distingish between 3 classes. 'index' has to be divided by '2', get step's number 
-    if now_class == '2':
+    #next version:  now_class has to be judged outside this function
+    if now_class == 2:
         level = math.ceil(index/2) + 5    
-    elif now_class == '3':
+    elif now_class == 3:
         level = math.ceil(index/2) + 10
     else:
         level = math.ceil(index/2)  
 
-    #put different datas into target csv
+    #put different datas into target csv 0==bottom  1==1_l  2==1_h 9==5_l 10==5_h
     if index == 0:
         with open(sep_f_path+'bottom.csv','w') as filewrite:
             writer = csv.writer(filewrite)
@@ -302,8 +300,12 @@ def write_each(filename, index):
                 writer = csv.writer(filewrite)
                 writer.writerows(filename)
 
-    else:
+    elif index == 11:
         with open(sep_f_path+'over.csv','w') as filewrite:
+            writer = csv.writer(filewrite)
+            writer.writerows(filename)
+    else:
+        with open(sep_f_path+'almost.csv','w') as filewrite:
             writer = csv.writer(filewrite)
             writer.writerows(filename)
 
@@ -356,27 +358,24 @@ def write_injson():
         with open(json_path + '{}.json'.format(csvname[:-4]), 'w') as f_wxid:
             f_wxid.write(data)
     print('All json files created successfully!!!')
-            
+
+all_names_dict = get_sys_data()      
 def start_job():
     global standard_target, num, end_num, now_class, miss_num
     now_class = get_class() 
     standard_target = now_class * 5
     level1_end_num = [202, 238, 172, 151, 178, 158]
     # 120 100 need to change right numbers!!
-    level2_end_num = [158, 149, 138, 120, 254, 205]
-    level3_end_num = [205, 100, 100, 133, 100, 226]
+    level2_end_num = [158, 120, 120, 120, 200, 205]
+    level3_end_num = [205, 120, 120, 120, 120, 226]
 
     end_num_list = [level1_end_num, level2_end_num, level3_end_num]
-
     num = get_wx()
     start_task()
-    write_in()
     output_csv()
     add2list(end_num_list[now_class - 1 ])
     create_json_task()
     write_injson()
-
-
 
 if __name__ == "__main__":
     start_job()
