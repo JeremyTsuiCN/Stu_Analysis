b9f2f957ebdf9a7ae81cb78fa7627feb1e7696c3
jeremy <564174495@qq.com>
2020-04-22 15:35:30 +0800

    补充了没有数据的学员通过课程后台调取当前关卡信息

--- forchange.py
+++ forchange.py
@@ -32,20 +32,20 @@ first_url2 = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-bac
 
 #update browser's headers, Next plan: [try to write in a txt file]
 todayheaders='''Host: prod.pandateacher.com
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:74.0) Gecko/20100101 Firefox/74.0
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:75.0) Gecko/20100101 Firefox/75.0
 Accept: application/json, text/plain, */*
 Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
 Accept-Encoding: gzip, deflate, br
-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4NTI4MjA1OSwiaWF0IjoxNTg1Mjc0ODU5LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.-CiJcqQG5NdMIhCS6ff962N3WehtN34WZYsIwPGkQiw
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4NzU0MzcyOCwiaWF0IjoxNTg3NTM2NTI4LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.ppUFxnIybVnltLRciPgFPj-AX1igr_CfRK0dyFQSXoU
 Connection: keep-alive
 Referer: https://prod.pandateacher.com/ninth-studio-future-railway/frontend/
-Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; locale=zh-CN; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4NTI4MjA1OSwiaWF0IjoxNTg1Mjc0ODU5LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.-CiJcqQG5NdMIhCS6ff962N3WehtN34WZYsIwPGkQiw
+Cookie: sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22obmqi1Px1ozjOic9rMfYzI2fD87s%22%2C%22%24device_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%2216dc975ec5afa-033c310891cf5a8-4a5b66-1764000-16dc975ec5b4aa%22%7D; locale=zh-CN; prod-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiJodHRwczovL2dpdC5mb3JjaGFuZ2UuY24vdXBsb2Fkcy8tL3N5c3RlbS91c2VyL2F2YXRhci80OTAvYXZhdGFyLnBuZyIsImV4cCI6MTU4NzU0MzcyOCwiaWF0IjoxNTg3NTM2NTI4LCJpc3MiOiJsd3JEekJhN1FEOGJ4OU8wSDF5N2lUT1R4ZGdQRE16YSIsIm5hbWUiOiLltJTkv4rmnbAiLCJzdWIiOjQ5MCwidW5hbWUiOiJjdWlqdW5qaWUifQ.ppUFxnIybVnltLRciPgFPj-AX1igr_CfRK0dyFQSXoU
 TE: Trailers'''
 
 headers = dict([line.split(": ", 1) for line in todayheaders.split("\n")])
 
 #special students' keyword in nicknames 
-keyword = ['忙忙', '考试', '放弃','请假', '周一', '周二', '周三', '周四', '周五', '周六', '周日', '周末','不回复', '自节奏', '私聊'] 
+keyword = ['忙忙', '考试', '放弃', '请假', '周末','不回复', '自节奏', '私聊', '抱佛脚'] 
 
 
 work = Queue()
@@ -61,8 +61,6 @@ standard_target = 0
 num = 0
 end_num = 0
 now_class = 0
-# '山脚'level needs to learn 6 tests, others needs 5
-miss_num = 5 
 
 
 #determine class level, return the answer to standard_target
@@ -88,10 +86,13 @@ def get_wx():
         delFile()
         print('Headers had updated, please copy a new one and run again!!')
         exit()
-
+    all_names_dict.pop('微信id')
     for studata in all_names_dict:
-        status_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/buddy/wxid_8kskqqw585ri12/{}?current_path=%2Fpanel%2Fconductor&is_group=false'.format(studata)
-        work.put_nowait(status_url)
+        if studata:
+            status_url = 'https://prod.pandateacher.com/ninth-studio-future-railway/base-backend/conductor/buddy/wxid_sqin1b8axidb22/{}?current_path=%2Fpanel%2Fconductor&is_group=false'.format(studata)
+            work.put_nowait(status_url)
+        else:
+            print('有学员退出群聊，没有微信数据的情况')
 
     #wanna print schedule, so I need this num to check presentage
     return len(all_names_dict)
@@ -114,7 +115,7 @@ def delFile():
 #before we catch info, we need to prevent railway system missing data, so we 
 #supplement complete table of data from 'all_names.csv'
 
-#return a dict=> key: wxid, value[0]: schedule, value[1]: nickname
+#return a dict=> key: wxid； value[0]: schedule； value[1]: nickname
 def get_sys_data():
     try:
         with open(abs_path + 'all_names.csv') as allwx_file:
@@ -134,42 +135,37 @@ def crawler():
         url = work.get_nowait()
         res2 = requests.get(url, headers = headers)
         res_json = res2.json()
-        try:
-            weixin = res_json['data']['wxid']
-        except TypeError:
-            return None
 
+        weixin = re.findall(r'.*\/(.*?)\?current_path', url)[0]
         nickname = res_json['data']['nickname']
         info = res_json['data']['current_train_info']
-        
         try:
             progress = info['progress']
             level = re.findall(r'第(.*?)课', progress)[0]
             distance = re.findall(r'#(.*?)句', progress)[0]
-        except (KeyError,IndexError):
-            try:
-                level =  miss_num - int(all_names_dict[weixin][0]) + ((now_class - 1) * 5)
-                progress = '无数据'
-                nickname = all_names_dict[weixin][1] + ' 无数据私聊'
-            except KeyError:
-                print(weixin+'NOT FOUND')
-                return None
-            finally:
-                if int(all_names_dict[weixin][0]) == 0:
-                    distance = 300
-                else:
-                    progress = ''
-                    distance = 0
+        except (KeyError, IndexError):
+            progress = '无'
 
-        except UnboundLocalError:
-            return None  
+        if progress == '无':
+            if now_class == 1:
+                miss_num = 6
+            else:
+                miss_num = 5
+            level =  miss_num - int(all_names_dict[weixin][0]) + ((now_class - 1) * 5)
+            progress = '无数据'
+            distance = 1
 
+        try:
+            if int(all_names_dict[weixin][0]) == 0:
+                distance = 300
+        except:
+            return None
 
         #determine whether any keyword is included in nickname
         if int(level) >= standard_target:
             normal_stu.append([nickname, weixin, progress, level, distance])
 
-        elif list( filter(lambda x: x in nickname,keyword )): 
+        elif list( filter(lambda x: x in nickname,keyword)): 
             private_stu.append([nickname, weixin, progress, level, distance])
 
         else:
@@ -177,6 +173,8 @@ def crawler():
 
         print("\b"*30,end="",flush=True)
         print('sum: {}, process: {:.1f}%'.format(num,100*(COUNT_INDEX/num) ), end="")
+
+
 
 def start_task():        
     task_list = []
@@ -193,7 +191,6 @@ def output_csv():
         writer.writerows(normal_stu)
 
     print('\n\nWrite in all students in this train successful!!\n\n')
-    # act_wxnames = dict( (line[1],line[0]) for line in normal_stu )
 
     if private_stu:
         with open(abs_path + 'per_con.csv', 'w', newline='') as perfile:
@@ -201,28 +198,10 @@ def output_csv():
             writer.writerow(['nickname', 'wxid', 'details', 'level', 'progress', 'learning status'])
             writer.writerows(private_stu)
         print('Write in special students successful!!')
-        # pri_wxid = [line[1] for line in private_stu]
     else:
-        # pri_wxid = []
         print('None special student catched!!! If keyword only 特殊, should updata url')
 
             
-    #Gather all students you can contact
-    # effective_stu = set(pri_wxid + [wx for wx in act_wxnames])
-    # missfile_path = createpath(abs_path + 'miss/')
-    #student can't be contacted, write in [miss_stu.csv] 
-    #all_names.csv 的顺序是 0wxid 1nickname 
-    # with open(missfile_path+'miss_stu.csv', 'w') as miss_file:
-    #     writer = csv.writer(miss_file)
-    #     for wxid in all_names_dict:
-    #         if wxid not in effective_stu:
-    #             writer.writerow([wxid,all_names_dict[wxid]])
-
-
-        # writer.writerows(miss_stu)
-
-    print('All csvs had generated successfully~~~')
-
 
 # One class has to set 12 groups students
 level_bottom = []
@@ -383,16 +362,16 @@ def write_injson():
             f_wxid.write(data)
     print('All json files created successfully!!!')
 
-all_names_dict = get_sys_data()      
+   
 def start_job():
-    global standard_target, num, end_num, now_class, miss_num
+    global standard_target, num, end_num, now_class
     now_class = get_class() 
     standard_target = now_class * 5
     level1_end_num = [202, 120, 120, 120, 120, 158]
     # 120 100 need to change right numbers!!
     level2_end_num = [158, 120, 120, 120, 200, 205]
 
-    level3_end_num = [205, 100, 120, 120, 120, 226]
+    level3_end_num = [205, 100, 90, 100, 120, 226]
 
     end_num_list = [level1_end_num, level2_end_num, level3_end_num]
     num = get_wx()
@@ -402,6 +381,47 @@ def start_job():
     create_json_task()
     write_injson()
 
+
+def getLazy():
+    day_n = 5
+    with open(abs_path + 'all_names.csv') as alldatafile:
+        reader = csv.reader(alldatafile)
+        alldata = dict((line[0], line[1:]) for line in reader)
+
+    verylazy = []
+
+    with open(abs_path + 'students_in_rail.csv') as getnamefile:
+        reader = csv.reader(getnamefile)
+        #微信id对应：0昵称  1进度高低值
+        act_data = dict((i[1],[i[0],i[4]]) for i in reader)
+
+    for wxid, miss in alldata.items():
+        if miss[7] == '0':
+            continue
+        delay_day =  day_n - int(miss.index(miss[day_n]))
+        if delay_day >= 2:
+            if wxid in act_data:
+                nickname = act_data[wxid][0]
+                if int(act_data[wxid][1]) > 90:
+                    miss[7] = int(miss[7]) - 1
+            else:
+                nickname = miss[0]
+
+            verylazy.append([wxid, nickname, miss[7], delay_day])
+
+    with open(abs_path + '不学习的.csv', 'w') as lazyfile:
+        writer = csv.writer(lazyfile)
+        writer.writerow(['微信id', '微信昵称', '还差多少关', '多少天没动'])
+        writer.writerows(verylazy)
+
+
+
+                    
+            
+        
+ 
+
 if __name__ == "__main__":
+    all_names_dict = get_sys_data()   
     start_job()
-    
+    # getLazy()
